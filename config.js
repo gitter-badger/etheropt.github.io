bab5a90420c130d748e9e3771f4d409cffe71596
frenchhoudini <frenchhoudini@gmail.com>
2016-03-14 00:36:35 -0500

    update

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.contract_market = 'market.sol';
 config.contract_market_addr = 'TBD';
 config.eth_testnet = false;
 config.eth_provider = 'http://localhost:8545';
-config.eth_addr = '0x0000000000000000000000000000000000000000';
+config.eth_addr = '0x0000000000000000000000000000000000000000'; 
 config.eth_addr_pk = '';
 
 try {
